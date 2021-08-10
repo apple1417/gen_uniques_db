8b6a7f22ea72c398b5bdeafcb0143be4cf3d37d1
apple1417 <git@apple1417.dev>
2021-08-11 00:07:19 +1200

    update website json script to add item list

--- demo/gen_website_json.py
+++ demo/gen_website_json.py
@@ -3,13 +3,46 @@ import sqlite3
 
 from util import DB_PATH, get_all_item_names, get_item_description, get_item_manufacturers
 
-OUTPUT_FILE = "uniques.json"
-
-all_sources = []
+ITEMS_FILE = "items.json"
+SOURCES_FILE = "sources.json"
 
 con = sqlite3.connect(DB_PATH)
 cur = con.cursor()
 
+all_items = []
+cur.execute("""
+select
+    i.ID,
+    i.Rarity,
+    i.GearCategory,
+    i.RequiredClass,
+    i.ItemGroup
+from
+    Items as i
+""")
+for (
+    item_id,
+    rarity,
+    gear_category,
+    req_class,
+    item_group
+) in cur.fetchall():
+    all_items.append([
+        get_all_item_names(con, item_id),
+        get_item_description(con, item_id, rarity, gear_category, req_class),
+        rarity,
+        get_item_manufacturers(con, item_id),
+        req_class,
+        gear_category,
+        item_group,
+    ])
+
+
+with open(ITEMS_FILE, "w") as file:
+    json.dump({"data": all_items}, file, separators=(",", ":"))
+
+
+all_sources = []
 cur.execute("""
 select
     i.ID,
@@ -48,5 +81,6 @@ for (
         item_group,
     ])
 
-with open(OUTPUT_FILE, "w") as file:
+
+with open(SOURCES_FILE, "w") as file:
     json.dump({"data": all_sources}, file, separators=(",", ":"))
